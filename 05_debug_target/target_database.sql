711e4b629658389a817c6cfd91bfb74c112c7531
Claudine Uwitije <uwiclaudia@gmail.com>
2026-06-20 11:47:42 +0000

    reorganize folder for debug target

---
+++ 05_debug_target/target_database.sql
@@ -0,0 +1,3 @@
+SELECT target_database 
+FROM `healthcare-data-project-499612.dev_healthcare_project_data.debug_target` 
+LIMIT 1000
---
+++ 05_debug_target/target_name.sql
@@ -0,0 +1,3 @@
+SELECT target_name, target_schema, target_database, target_name 
+FROM `healthcare-data-project-499612.dev_healthcare_project_data.debug_target` 
+LIMIT 1000
